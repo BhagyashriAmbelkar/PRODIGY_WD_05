c273e5167063a41fafa42d8df1db428e2452272b
BhagyashriAmbelkar <164602717+BhagyashriAmbelkar@users.noreply.github.com>
2024-03-30 10:45:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+
+    const apiKey="8569d6770f7c827b23d217d0545ed724";
+    const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+    const searchBox = document.querySelector( ".search input");
+    const searchBtn = document.querySelector( ".search button");
+    const weatherIcon = document.querySelector(".weather-icon");
+
+
+    async function chackWhether(city){
+      const response = await  fetch(apiUrl + city + `&appid=${apiKey}`);
+
+    if(response.status == 404) {
+      document.querySelector(".error").style.display="block";
+      document.querySelector(".weather").style.display="none";
+    }
+    else{
+      var data = await response.json();
+      console.log(data);
+  
+      document.querySelector(".city").innerHTML= data.name;
+      document.querySelector(".temp").innerHTML= Math.round(data.main.temp)+"<sup>o</sup>C";
+      document.querySelector(".humidity").innerHTML= data.main.humidity + "%";
+      document.querySelector(".wind").innerHTML= data.wind.speed + " km/hr";
+      
+      
+      if(data.weather[0].main =="Clouds"){
+        weatherIcon.src="img/clouds.png";
+      }
+      else if(data.weather[0].main =="Clear"){
+        weatherIcon.src="img/clear.png";
+      }
+      else if(data.weather[0].main =="Drizzle"){
+        weatherIcon.src="img/drizzle.png";
+      }
+      else if(data.weather[0].main =="Rain"){
+        weatherIcon.src="img/rain.png";
+      }
+      else if(data.weather[0].main =="Haze"){
+        weatherIcon.src="img/haze.png";
+      }
+      else if(data.weather[0].main =="Snow"){
+        weatherIcon.src="img/snow.png";
+      }
+  
+      document.querySelector(".weather").style.display = "block";
+      document.querySelector(".error").style.display="none";
+
+    }
+
+  }
+    searchBtn.addEventListener("click", ()=>{
+      chackWhether(searchBox.value);
+    })
